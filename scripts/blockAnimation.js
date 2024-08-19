9c1c22e4afc3bb5753f0f616a8701026e43e2718
Айдын <aidyn@MacBook-Pro-David.local>
2024-08-19 10:46:49 +0500

    feat: final

--- scripts/blockAnimation.js
+++ scripts/blockAnimation.js
@@ -6,6 +6,26 @@ document.addEventListener('DOMContentLoaded', function(){
     let fourth = document.getElementById('fourthAnimated');
     let fifth = document.getElementById('fifthAnimated');
     let greenMark = document.getElementById('greenAnimated');
+    let goalItem1 =document.getElementById('goalItem1');
+    let goalItem2 =document.getElementById('goalItem2');
+    let goalItem3 =document.getElementById('goalItem3');
+    let goalItem4 =document.getElementById('goalItem4');
+    let openGoal = document.getElementById('openGoal');
+    let programm = document.getElementById('programm');
+    let openProgramm = document.getElementById('openProgramm');
+
+    openGoal.addEventListener("click",() => {
+        goalItem1.style.display = 'flex';
+        goalItem2.style.display = 'flex';
+        goalItem3.style.display = 'flex';
+        goalItem4.style.display = 'flex';
+        openGoal.style.display = 'none';
+    })
+
+    openProgramm.addEventListener("click",() => {
+        programm.style.display = 'flex';
+        openProgramm.style.display = 'none';
+    })
     
     setTimeout(function(){
         first.style.top = '0';
